26ec6f32d1a4a2ec223f20c113635a481455802e
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-01-06 11:23:49 +0200

    GCD using Euclids Algorithm

---
+++ algorithm_practice/euclidAlgo/gcd.py
@@ -0,0 +1,16 @@
+# Greatest Common Divisor using Euclid's Algorithm
+
+def gcd(x, y):
+
+    larger = max(x, y)
+    smaller = min(x, y)
+
+    remainder = larger % smaller
+
+    if remainder == 0:
+        return (smaller)
+
+    if remainder != 0:
+        return (gcd(smaller, remainder))
+    
+print(gcd(89, 59))
