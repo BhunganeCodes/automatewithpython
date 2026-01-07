c15e5fa0d19246d701d4747c66ad835b312e839d
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-01-07 18:39:08 +0200

    Insertion Sort Algorithm

---
+++ algorithm_practice/sortSearchAlgo/insertion.py
@@ -0,0 +1,17 @@
+# Insertion Sort Algorithm
+
+def insert_cabinet(cabinet, to_insert):
+    check_location = len(cabinet) - 1
+    insert_location = 0
+    
+    while check_location >= 0:
+        if to_insert > cabinet[check_location]:
+            insert_location = check_location + 1
+            check_location = -1
+        check_location -= 1
+    cabinet.insert(insert_location, to_insert)
+    return cabinet
+
+cabinet = [1, 2, 3, 3, 6, 8, 12]
+new_cabinet = insert_cabinet(cabinet, 5)
+print(new_cabinet)
