37fe6c22a4d9fd9d769c117775622eaceab612be
Thamsanqa Hadebe <thhadjhb025@student.wethinkcode.co.za>
2026-01-13 07:09:15 +0200

    Sleep sort algorithm implementation whilst ignoring the result in phase one

--- algorithm_practice/sortSearchAlgo/sleepSort.py
+++ algorithm_practice/sortSearchAlgo/sleepSort.py
@@ -1,1 +1,15 @@
 # To implement sleep sort algorithm below...
+import threading
+from time import sleep
+
+def sleep_sort(i):
+    global sorted_list
+    sleep(i)
+    sorted_list.append(i)
+    return i
+
+items = [2, 4, 5, 2, 1, 7]
+sorted_list = []
+ignore_result = [threading.Thread(target=sleep_sort, args=(i,)).start() for i in items]
+
+print(ignore_result)
